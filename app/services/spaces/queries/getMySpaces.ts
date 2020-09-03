67fe7aa4520781d25e2d1cd56055f08e27a36d44
Gabriel Colson <gabrielcolson@hotmail.fr>
2020-09-03 03:20:17 +0200

    fix ts

--- app/services/spaces/queries/getMySpaces.ts
+++ app/services/spaces/queries/getMySpaces.ts
@@ -3,7 +3,6 @@ import db, { Space } from "db"
 
 export type SpaceWithUsers = Space & {
   owner: { id: string; name: string; pictureURL: string | null }
-} & {
   members: { id: string; name: string; pictureURL: string | null }[]
 }
 
